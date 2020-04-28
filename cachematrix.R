af254d2b49f12a1a7b7d04fd5daae4b9cdb59758
Hui Ying <huiying@live.co.uk>
2020-04-28 08:27:07 +0800

    28.4.2020

--- cachematrix.R
+++ cachematrix.R
@@ -1,33 +1,45 @@
-
+#make cache matrix
 makeCacheMatrix <- function(x = matrix()) {
+  #initialize 
   m<- NULL
+  #set the matrix
   set <-function(matrix){
     x<<-matix
     m<<-NULL
   }
+  #get the matrix
   get<- function(){
     x
   }
+  #inverse the matrix
   setInverse <- function(inverse) {
     m <<- inverse
   }
+  #get inverse of matrix
   getInverse <- function() {
     m
   }
+  #return list
   list(set = set, get = get,
        setInverse = setInverse,
        getInverse = getInverse)
 }
 
+##cachesolve
 
 cacheSolve <- function(x, ...) {
+  #return inverse matrix
   m <- x$getInverse()
+  #return the inverse if it is already set
   if( !is.null(m) ) {
     message("getting cached data")
     return(m)
   }
+  #get matrix
   data <- x$get()
+  #calculate inverse
   m <- solve(data,...)
+  #set inverse of object
   x$setInverse(m)
   m
 }
